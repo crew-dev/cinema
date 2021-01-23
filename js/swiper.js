2dc6b4d04e5e5e1b03c4813a40c8a6dc4bf31c96
Vladislav Dedikov <69351396+crew-dev@users.noreply.github.com>
2021-01-23 17:43:44 +0300

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper(".swiper-container", {
+  slidesPerView: 3,
+  loop: true,
+	autoplay: true,
+	disableOnInteraction: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
